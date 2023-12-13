b76d23a2f03abb894a08eed874dfcce0a0f0e78d
letvarprint <a.brixencov@icloud.com>
2023-12-13 05:35:18 +0200

    Initial Commit

---
+++ svetofor/svetoforApp.swift
@@ -0,0 +1,17 @@
+//
+//  svetoforApp.swift
+//  svetofor
+//
+//  Created by Alexandru Brixencov on 13.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct svetoforApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
